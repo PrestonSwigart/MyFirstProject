53d72bd75f2d8a248eddf4a98bd7c80cfc4bda42
Preston Swigart <prestonlswigart@gmail.com>
2022-09-13 09:19:24 -0400

    commit message

--- src/Bobby.java
+++ src/Bobby.java
@@ -1,6 +1,45 @@
 public class Bobby {
-    //encapsulation
+    // encapsulation
     private String name;
-    private int age;
-    private double coolnessFactor; //camelCase
+    private int age;    // primitive data: holdover from C
+    private double coolnessFactor;  // camelCase
+
+    /*
+    primitives from C/C++
+    int (short, int, long), double (float), boolean, char
+     */
+    private final double PI = 3.141578;
+
+    /**
+     * Parameterized constructor for class Bobby
+     * @param n name
+     * @param a age
+     * @param c coolnessFacor
+     */
+    public Bobby(String n, int a, double c) {
+        name = n;       // ASSIGNMENT OPERATOR -- right-associative
+        age = a;
+        coolnessFactor = c;
+    }
+
+    // ACCESSOR METHODS
+    public String getName()     {   return name;    }
+    public int getAge()         {   return age;     }
+    /**
+     * public return of coolnessFactor
+     * @return a double representing how cool this Bobby is
+     */
+    public double getCoolness() {   return coolnessFactor;  }
+
+    public String toString()    {
+        return name + " is " + age + " years old and is " +
+                (coolnessFactor*100) + "% cool";
+    }
+
+    public static void main(String[] args) {
+        Bobby b = new Bobby("PlaceholderName", 453, .98);
+        System.out.println(b);
+        System.out.println(b.getCoolness());
+    }
 }
+
